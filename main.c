ef57acf3e28480508d2f9adedd88d6547ef55be5
SNA01IU412B <56293350+SNA01IU412B@users.noreply.github.com>
2020-09-23 16:41:27 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct node{
+    int val; // Just value
+    struct node* next; // Points to the next node
+} node_t;
+
+//Functions
+
+void push(node_t **head, int data); //Add a node
+void pop(node_t** head); //Delete the "top" node
+void list(node_t* head); //Just... List IDK
+
+
+int main(void) {
+    int i = 0;
+    int a = 0;
+    node_t* head = (node_t *) calloc(sizeof(node_t), 1);
+    printf("What do you want from me?\n");
+    printf("1 - Push\n");
+    printf("2 - Pop\n");
+    printf("3 - List\n");
+    printf("4 - End\n");
+    scanf("%d", &i);
+    while(1) {
+        switch (i) {
+            case (1):
+                printf("Enter some number\n");
+                scanf("%d", &a);
+                push(&head, a);
+                printf("What's next?\n");
+                scanf("%d", &i);
+                break;
+            case (2):
+                pop(&head);
+                printf("What's next?\n");
+                scanf("%d", &i);
+                break;
+            case (3):
+                list(head);
+                scanf("%d", &i);
+                break;
+            case (4):
+                printf("Goodbye\n");
+                exit(0);
+        }
+    }
+    return (0);
+}
+
+void push(node_t** head, int data) {
+    node_t* tmp = (node_t*)calloc(sizeof(node_t),1);
+    tmp->val = data;
+    tmp->next = *head;
+    *head = tmp;
+}
+
+void pop(node_t** head) {
+    node_t* prev = NULL;
+    if (head == NULL) { // IF ITS NOT THE LAST ONE
+        printf("This node can not be deleted");
+        exit(0);
+    }
+    prev = *head;
+    *head = (*head)->next;
+    free(prev); // Free the memory of a deleted node
+}
+
+void list(node_t* head) {
+    if (head->next ==0) printf("No objects in a list");
+    while (head->next !=0) {
+        printf("%d ", head->val);
+        head = head->next;
+    }
+    printf("\n");
+}
